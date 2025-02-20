294d0fbbb2bfa7336ae7ff8ee40f43917f73757f
goksugl <gulizar.goksu123@gmail.com>
2025-02-20 23:10:02 +0300

    Net&ReactFullStackOdev1

---
+++ NvRFS1_1.sayı/NvRFS1_odev1_sayı/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 1. Sayının Pozitif, Negatif veya Sıfır Olduğunu Belirleme
+
+    Console.Write("Bir sayı girin: ");
+    double number = Convert.ToDouble(Console.ReadLine());
+
+    if (number > 0) Console.WriteLine("Pozitif sayı.");
+    else if (number < 0) Console.WriteLine("Negatif sayı.");
+    else Console.WriteLine("Sıfır.");
---
+++ NvRFS1_4.enbuyuksayı/NvRFS1_enbuyuksayı/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 4. Üç Sayının En Büyüğünü Bulma
+
+    Console.Write("Birinci sayıyı girin: ");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("İkinci sayıyı girin: ");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Üçüncü sayıyı girin: ");
+    double num3 = Convert.ToDouble(Console.ReadLine());
+
+    double max = num1;
+    if (num2 > max) max = num2;
+    if (num3 > max) max = num3;
+
+    Console.WriteLine($"En büyük sayı: {max}");
+
